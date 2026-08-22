0b44c29820de03f648b6bb1a808353868b7104b6
veraperez23 <topclasetrabajos@gmail.com>
2026-08-22 21:26:41 +0200

    saved graphs2

--- run.py
+++ run.py
@@ -71,7 +71,7 @@ if __name__=='__main__':
     else:
         transform_train = AllSky()
 
-    train_dataset = CloudDataset(folder_path=train_dir, txt_path="./datos/train_day.txt", transform=transform_train)
+    train_dataset = CloudDataset(folder_path=train_dir, txt_path="./datos/train.txt", transform=transform_train)
     
     # if cfg.train.refine_flag == True: #no lo usamos en este proyecto
 
@@ -104,7 +104,7 @@ if __name__=='__main__':
 
     validation_dir = cfg.validation.val_dir
     transform_val = transforms.Compose([transforms.Resize((512, 512), interpolation=transforms.InterpolationMode.BILINEAR), transforms.ToTensor()])
-    val_dataset = CloudDataset(folder_path=validation_dir, txt_path="./datos/val_day.txt", transform=transform_val)
+    val_dataset = CloudDataset(folder_path=validation_dir, txt_path="./datos/val.txt", transform=transform_val)
     val_dataloader = DataLoader(val_dataset, batch_size=cfg.validation.batch_size, shuffle=True, num_workers=cfg.validation.num_workers, pin_memory=True, drop_last=False)
 
     ###### TEST DATASET DAY ######
